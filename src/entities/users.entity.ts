e7114c16b53bc0875f6b6b67c15c3af7ebbf5fbe
nutthanon <nutthanon.tho@gmail.com>
2024-01-10 13:41:03 +0700

    fix: semi-colon

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -9,10 +9,9 @@ import {
 } from 'typeorm';
 import { Follows } from './follows.entity';
 import { UserFriendLists } from './user_friend_list.entity';
-import { Memories } from './memory_card.entity'
+import { Memories } from './memory_card.entity';
 import { Albums } from './albums.entity';
 import { Exclude } from 'class-transformer';
-
 
 @Entity()
 export class Users extends BaseEntity {
@@ -32,17 +31,23 @@ export class Users extends BaseEntity {
   @OneToMany(() => Follows, (follows) => follows.following)
   following: Follows[];
 
-  @OneToMany(() => UserFriendLists,(user_friend_lists) => user_friend_lists.user_id,)
+  @OneToMany(
+    () => UserFriendLists,
+    (user_friend_lists) => user_friend_lists.user_id,
+  )
   user_friend_lists: UserFriendLists[];
 
-  @OneToMany(() => UserFriendLists,(user_friend_lists) => user_friend_lists.user_in_list,)
+  @OneToMany(
+    () => UserFriendLists,
+    (user_friend_lists) => user_friend_lists.user_in_list,
+  )
   user_friend_lists_in_list: UserFriendLists[];
 
-  @OneToMany(() => Memories,(memory_card) => memory_card.user_id)
-  memory_card:Memories[];
+  @OneToMany(() => Memories, (memory_card) => memory_card.user_id)
+  memory_card: Memories[];
 
-  @OneToMany(() => Albums,(albums) => albums.user_id)
-  album_id:Albums[];
+  @OneToMany(() => Albums, (albums) => albums.user_id)
+  album_id: Albums[];
 
   @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP(6)', update: false })
   created_at: Date;
